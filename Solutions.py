b9a954d4c5256e18661ca24ee2135fc0aea35351
Shashank Srivastava <shashank.normative@gmail.com>
2017-01-11 14:51:07 -0500

    feat: Given a graph find minimum spanning tree. Prim's algo implemented
    Resolves #Question 3

--- Solutions.py
+++ Solutions.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+from heapq import *
 class Solutions:
 	def question1(self,s, t):
 		string1=sol.question1_helper(s)
@@ -74,7 +76,26 @@ class Solutions:
 		print "LPS of string is " + a + " : ",
 		print a[start:end+1],
 		print "\n",
-		
+	def question3(self,nodes, edges ):
+		conn = defaultdict( list )
+		for n1,n2,c in edges:
+			conn[ n1 ].append( (c, n1, n2) )
+			conn[ n2 ].append( (c, n2, n1) )
+
+		mst = []
+		used = set( [nodes[ 0 ]] )
+		usable_edges = conn[ nodes[0] ][:]
+		heapify( usable_edges )
+
+		while usable_edges:
+			cost, n1, n2 = heappop( usable_edges )
+			if n2 not in used:
+				used.add( n2 )
+				mst.append( ( n1, n2, cost ) )
+				for e in conn[ n2 ]:
+					if e[ 2 ] not in used:
+						heappush( usable_edges, e )
+		return mst	
 		
 sol=Solutions()
 # Q1 INPUT 1
@@ -88,6 +109,14 @@ sol=Solutions()
 # Q2 INPUT 3
 #sol.question2('abacdfgdcaba')			
 # Q2 INPUT 4
-#sol.question2('abacdedcaba')			
-
+#sol.question2('abacdedcaba')
+nodes = list("ABCDEFG")
+edges = [ ("A", "B", 7), ("A", "D", 5),
+          ("B", "C", 8), ("B", "D", 9), ("B", "E", 7),
+      ("C", "E", 5),
+      ("D", "E", 15), ("D", "F", 6),
+      ("E", "F", 8), ("E", "G", 9),
+      ("F", "G", 11)]			
+#sol.prim( nodes, edges )
+print "prim:", sol.question3( nodes, edges )
 	
