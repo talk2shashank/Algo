527ed8917e553ee11865c8820e43df5e34a4c5bf
Shashank Srivastava <shashank.normative@gmail.com>
2017-01-15 01:06:42 -0500

    refactor: logic to calculate lowest common ancestor deleted

--- Solutions.py
+++ Solutions.py
@@ -114,19 +114,7 @@ class Node1:
 			return True
 		path.pop()
 		return False
-	def findLCA(root, n1, n2):
-		path1 = []
-		path2 = []
-		if (not findPath(root, path1, n1) or not findPath(root, path2, n2)):
-			return -1
-		i = 0
-		while(i < len(path1) and i < len(path2)):
-			if path1[i] != path2[i]:
-				break
-			i += 1
-		return path1[i-1]
-	def question4(self,T, r, n1, n2):
-		n.findLCA()
+	
 		
 llist = LinkedList()
 sol=Solutions()
