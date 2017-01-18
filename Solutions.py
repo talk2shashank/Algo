a8117a0654a47203c6fc7d14db20cd886be7d15e
Shashank Srivastava <shashank.normative@gmail.com>
2017-01-18 11:28:53 -0500

    feat: DeleteNode function added for node 1 class

--- Solutions.py
+++ Solutions.py
@@ -159,6 +159,31 @@ class Node1:
 			return True
 		path.pop()
 		return False
+	def DeleteNode(self,string):
+		maxLength = 1
+		start = 0
+		length = len(string)
+		low = 0
+		high = 0
+		for i in xrange(1, length):
+			low = i - 1
+			high = i
+			while low >= 0 and high < length and string[low] == string[high]:
+				if high - low + 1 > maxLength:
+					start = low
+					maxLength = high - low + 1
+				low -= 1
+				high += 1
+			low = i - 1
+			high = i + 1
+			while low >= 0 and high < length and string[low] == string[high]:
+				if high - low + 1 > maxLength:
+					start = low
+					maxLength = high - low + 1
+				low -= 1
+				high += 1
+		print string[start:start + maxLength]
+		
 	def longestPath( root, path, k):
 		if root is None:
 			return False
